3ef301c48d695990d2cce2e16a98b283482550cd
김어진 <linexy@naver.com>
2022-08-04 00:08:14 +0900

    Feat: InitDB 수정

--- src/main/java/com/zeroback/aboutme/util/InitDB.java
+++ src/main/java/com/zeroback/aboutme/util/InitDB.java
@@ -1,12 +1,16 @@
 package com.zeroback.aboutme.util;
 
 import com.zeroback.aboutme.entity.Member;
+import com.zeroback.aboutme.entity.MemberInfo;
+import com.zeroback.aboutme.entity.MemberTag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -25,8 +29,19 @@ public class InitDB {
         private final EntityManager em;
         public void dbInit1() {
             Member member = Member.createMember("김어진", "test@test.com", "test");
+            member.setContent("안녕하세요");
+            member.setJob("개발자");
+            member.setPhone("010-2341-1411");
             em.persist(member);
+            MemberTag.create(member,"백엔드");
+            MemberTag.create(member,"프론트");
+            MemberTag.create(member,"안드로이드");
 
+            List<MemberInfo> memberInfoList = member.getMemberInfo();
+            List<String> infos = Arrays.asList("요리를 잘한다", "일을 뒤로 미룬다","잘때는 건들지 말기!","ABCD","고기","해산물","부드러움","컴퓨터");
+            for(int i = 0 ; i<8;i++){
+                memberInfoList.get(i).setContent(infos.get(i));
+            }
 
         }
     }
